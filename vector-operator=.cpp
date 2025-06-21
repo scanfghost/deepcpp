7f90994302eef24aaa015eb2d91abdb5aceec138
huangzonghao <3614167154@qq.com>
2025-06-21 12:58:34 +0800

    three modes to utilize operator=

--- vector-operator=.cpp
+++ vector-operator=.cpp
@@ -2,18 +2,31 @@
 #include<iostream>
 
 int main(){
-    std::vector<int> x({1,2,3});
+    std::vector<int> x({1,2,3,4,5});
 
     //copy 
     std::vector<int> first=x;
-    std::cout<<"copy vector first elements:";
+    std::cout<<"copy= vector first elements:";
     for(std::vector<int>::iterator it = first.begin();it != first.end();it++){
         std::cout<<*it<<" ";
     }
     std::cout<<"\n";
 
     //move 
-    std::vector<int> second
+    std::vector<int> second_1 = std::move(x);
+    std::cout<<"move= vector second_1 elements:";
+    for(std::vector<int>::iterator it = second_1.begin();it != second_1.end();it++){
+        std::cout<<" "<<*it;
+    }
+    std::cout<<"\n";
+    /* vector is left in an unspecified but valid
+    std::vector<int> second_2 = std::vector<int>(x.rbegin()+1,x.rend()-1);{4,3,2}*/
+    std::vector<int> second_2 = std::vector<int>(second_1.rbegin()+1,second_1.rend()-1);
+    std::cout<<"move= vector second_2 elements:";
+    for(std::vector<int>::iterator it = second_2.begin();it != second_2.end();it++){
+        std::cout<<" "<<*it;
+    }
+    std::cout<<"\n";
 
     return 0;
 }
